3577aeb2b4dfebdb5b808a0b825eb29176c50373
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-18 21:08:30 +0000

    Add posts section in the blog/views.py file

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+    'author': 'DK76Yuss',
+    'title': 'Blog Post 1',
+    'content': 'This is my first blog post',
+    'date_posted': '18th December, 2024',
+    },
+    {
+    'author': 'MettPro26',
+    'title': 'Blog Post 2',
+    'content': 'This is my second blog post',
+    'date_posted': '20th December, 2024',
+    }
+]
+
 def welcome(request):
-    return render(request, 'blog/welcome.html')
+    context = {
+        'articles': posts
+    }
+    return render(request, 'blog/welcome.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')

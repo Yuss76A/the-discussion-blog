8883d9da43493a70588321c4513d6a4ed5d04f52
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-18 20:51:53 +0000

    Create a templates folder, containing about.html and welcome.html in the blog app

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def welcome(request):
-    return HttpResponse("<h1>Discussion Blog</h1>")
+    return render(request, 'blog/welcome.html')
 
 def about(request):
-    return HttpResponse("<h1>What This Blog Is About</h1>")
+    return render(request, 'blog/about.html')

490b8f84b26bb35b773211c9789ce32ae76e6ffc
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-25 11:49:38 +0000

    Refactor user management: Rename model, update forms, and fix URLs

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Account
 
 class UserRegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=100)
@@ -10,3 +11,22 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email',]
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']  # Users can update these fields
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if User.objects.filter(email=email).exists():
+            raise forms.ValidationError("This email is already associated with another account.")
+        return email
+
+class AccountUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Account  # This should be the model name for your user account
+        fields = ['avatar']  # Adjusted field
--- users/views.py
+++ users/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import login
 from django.contrib import messages
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, AccountUpdateForm 
 
 def register_view(request):
     if request.method == 'POST':
@@ -22,3 +23,27 @@ def register_view(request):
 @login_required
 def account(request):
     return render(request, 'users/account.html')
+
+
+@login_required
+def update_account(request):
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)  
+        account_form = AccountUpdateForm(request.POST, request.FILES, instance=request.user.useraccount)  
+
+        if user_form.is_valid() and account_form.is_valid():
+            user_form.save() 
+            account_form.save()  
+            messages.success(request, 'Your account has been successfully updated!')  
+            return redirect('account') 
+
+    else:
+        
+        user_form = UserUpdateForm(instance=request.user)
+        account_form = AccountUpdateForm(instance=request.user.useraccount)
+
+    context = {
+        'user_form': user_form,
+        'account_form': account_form
+    }
+    return render(request, 'users/edit_account.html', context)  

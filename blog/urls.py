92f45947eeea4a15fca60db9beceb20d49c6fd73
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-26 21:55:28 +0000

    Create post_detail.html and enhance article template for clarity and appearance

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
+from .views import PostListView, PostDetailView
 
 
 urlpatterns = [
-    path('', views.welcome, name="welcome"),
+    # path('', views.welcome, name="welcome"),
     path('about/', views.about, name="about"),
-    path('post/<int:id>/', views.post_detail, name='blog-detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="blog-detail"),
+    path('', PostListView.as_view(), name="blog-welcome")
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -3,13 +3,14 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import login
 from django.http import HttpResponse
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 
-def welcome(request):
-    context = {
-        'articles': Post.objects.all()
-    }
-    return render(request, 'blog/welcome.html', context)
+# def welcome(request):
+#     context = {
+#         'articles': Post.objects.all()
+#     }
+#     return render(request, 'blog/welcome.html', context)
 
 
 def about(request):
@@ -18,4 +19,13 @@ def about(request):
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id)  
-    return render(request, 'blog/post_detail.html', {'post': post})  
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/welcome.html'
+    context_object_name = 'articles'
+    ordering = ["-date_posted"]
+
+class PostDetailView(DetailView):
+    model = Post

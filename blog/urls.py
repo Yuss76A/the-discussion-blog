911e334f87bf6c579eed0d12385f476849808f28
Yusein Ali <yuse_91yus@hotmail.com>
2024-12-31 16:07:29 +0000

    Modify the trending window to display most recent posts

--- blog/urls.py
+++ blog/urls.py
@@ -6,8 +6,6 @@ from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView,
 
 # URL patterns for blog application navigation
 urlpatterns = [
-    # path('', views.welcome, name="welcome"),
-
     path('', PostListView.as_view(), name="welcome"),
     path('about/', views.about, name="about"),
     path('highlights/', views.highlights, name="highlights"),
@@ -15,9 +13,8 @@ urlpatterns = [
     path('about-me/', views.about_me, name='about-me'),
     path('post-new/', PostCreateView.as_view(), name="blog-new"),
     path('post/<int:pk>/', PostDetailView.as_view(), name="blog-detail"),
-    # path('post/<int:pk>/', post_detail, name="blog-detail"),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='blog-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name="blog-delete"),
-    path('comment/<int:comment_id>/update/', update_comment, name='update-comment'),  # Update comment URL
+    path('comment/<int:comment_id>/update/', update_comment, name='update-comment'),
     path('comment/<int:comment_id>/delete/', delete_comment, name='delete-comment'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,33 +8,10 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from .forms import CommentForm, CollaborateForm
 
-
-# def welcome(request):
-#     context = {
-#         'articles': Post.objects.all()
-#     }
-#     return render(request, 'blog/welcome.html', context)
-
-# About page view
+# About Page
 def about(request):
     return render(request, 'blog/about.html', {'title': "About Page"})
 
-
-# Post detail view
-# def post_detail(request, pk):
-#     post = get_object_or_404(Post, id=pk)  # Get the post by primary key
-#     comments = post.comments.all()  # Retrieve associated comments
-
-#     # Handle comment submission
-#     if request.method == 'POST':
-#         content = request.POST.get('content')
-#         if request.user.is_authenticated:
-#             Comment.objects.create(post=post, author=request.user, content=content)  # Create new comment
-#             messages.success(request, 'Your comment has been added!')
-#             return redirect('blog-detail', pk=post.id)  # Redirect to the detail view of the post
-
-#     return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments})
-    
 
 # List of all posts view
 class PostListView(ListView):
@@ -43,33 +20,38 @@ class PostListView(ListView):
     context_object_name = 'articles'
     ordering = ["-date_posted"]
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['trending_posts'] = Post.objects.order_by('-date_posted')[:5]  # Fetch trending posts
+        return context
+
 # Detail view for a specific post
 class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
-    context_object_name = 'post'  # Name the context object for access in the template
+    context_object_name = 'post'  
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = self.object.comments.all()  # Include comments in context
-        context['comment_form'] = CommentForm()  # Initialize the comment form
+        context['comments'] = self.object.comments.all()
+        context['comment_form'] = CommentForm()
         return context
 
     def post(self, request, *args, **kwargs):
         self.object = self.get_object()
-        form = CommentForm(request.POST)  # Create a form instance with submitted data
+        form = CommentForm(request.POST)
         
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = self.object  # Link the comment to the post
-            comment.author = request.user  # Set the comment author
-            comment.save()  # Save the comment
+            comment.post = self.object
+            comment.author = request.user
+            comment.save()
             messages.success(request, 'Your comment has been added!')
-            return redirect('blog-detail', pk=self.object.id)  # Redirect back to post detail
+            return redirect('blog-detail', pk=self.object.id)
 
         # If the form is not valid, render the same page with form errors
         context = self.get_context_data()
-        context['comment_form'] = form  # Include the invalid form to show errors
+        context['comment_form'] = form
         return self.render_to_response(context)
   
 

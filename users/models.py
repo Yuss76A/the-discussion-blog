e30960ce673d388b2f829b5b200facc24ceb4927
Yusein Ali <yuse_91yus@hotmail.com>
2025-01-10 22:56:31 +0000

    Add cloudinary configuration for store images

--- users/models.py
+++ users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from cloudinary.models import CloudinaryField
 
 class Account(models.Model):
     """
@@ -11,7 +11,7 @@ class Account(models.Model):
     avatar (ImageField): The profile image for the user, with a default image set.
     """  
     user = models.OneToOneField(User, on_delete=models.CASCADE)  
-    avatar = models.ImageField(default='default.jpg', upload_to='images/')  
+    avatar = CloudinaryField('image', default='default_avatar') 
 
     def __str__(self):
         return f"{self.user.username}'s Account"

4609fe583a8244d698afb12d19b4e8f00385bcb6
Yusein Ahmed <yuseinahmed@MacBook-Pro-de-Yusein.local>
2025-02-09 02:53:36 +0100

    Fix the links for the social media, control panel, and the user name in the post detail container, also fix the non articles available message and move it before pagination element

--- discussion_blog/urls.py
+++ discussion_blog/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from blog import views as index_views
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
-from django.conf import settings
-from django.conf.urls.static import static
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 # URL configuration for the Django project, mapping URL paths to their respective views for
 # the blog and user accounts functionalities, including admin access, authentication, and 
@@ -15,7 +14,7 @@ urlpatterns = [
     path('', include('blog.urls')),
     path('register/', user_views.register_view, name='register'),
     path('account/', user_views.account, name='account'),
-     path('account/update/', user_views.update_account, name='update_account'),
+    path('account/update/', user_views.update_account, name='update_account'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name="logout")
 ]

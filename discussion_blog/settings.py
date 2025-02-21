0f006d5da6f9051f65a4f6d59818d8fd6cc02c0e
Yusein Ahmed <yuseinahmed@MacBook-Pro-de-Yusein.local>
2025-02-21 15:43:49 +0100

    Modify the reset password section in the settings.py file

--- discussion_blog/settings.py
+++ discussion_blog/settings.py
@@ -155,6 +155,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('yuse_91yus@hotmail.com')
-    EMAIL_HOST_PASSWORD = os.environ.get('xfjf yfdu ajgz tmdo')
-    DEFAULT_FROM_EMAIL = os.environ.get('yuse_91yus@hotmail.com')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')

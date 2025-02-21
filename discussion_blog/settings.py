efff5d7b9444e669f68efbad28535cc7b1ceede4
Yusein Ahmed <yuseinahmed@MacBook-Pro-de-Yusein.local>
2025-02-21 15:49:45 +0100

    Modify the default_from_email section in the settings.py file

--- discussion_blog/settings.py
+++ discussion_blog/settings.py
@@ -157,4 +157,4 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')

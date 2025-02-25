432c3b5adf299ad82cc221418601bf0f46c6932a
Yusein Ahmed <yuseinahmed@MacBook-Pro-de-Yusein.local>
2025-02-25 03:25:32 +0100

    Add comments to the asgi.py file in the discussion blog directory

--- discussion_blog/asgi.py
+++ discussion_blog/asgi.py
@@ -2,6 +2,8 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+# Set the default setting module for the Django application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'discussion_blog.settings')
 
+# Initialize the ASGI application with the given settings
 application = get_asgi_application()
